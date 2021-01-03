0c785b16ceed810d54ca797276997473d15b0139
ivanvmoreno <contacto@ivanmoreno.me>
2021-01-03 19:59:23 +0100

    [feat] Refreshed architecture

---
+++ renew-certs.sh
@@ -0,0 +1,13 @@
+# https://certbot-dns-cloudflare.readthedocs.io/en/stable/
+
+# sudo snap install --classic certbot
+# ln -s /snap/bin/certbot /usr/bin/certbot
+# sudo snap set certbot trust-plugin-with-root=ok
+# snap install --beta certbot-dns-cloudflare
+# sudo snap connect certbot:plugin certbot-dns-cloudflare
+
+certbot certonly \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials ~/.secrets/cloudflare.ini \
+  --dns-cloudflare-propagation-seconds 60 \
+  -d "*.ivan.build"
