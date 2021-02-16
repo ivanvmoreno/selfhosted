c873fa8a0fce30a3ee2bf0c3b32977d5fa25c558
ivanvmoreno <contacto@ivanmoreno.me>
2021-02-16 11:22:47 +0100

    [chore] Updated workloads

--- scripts/issue-certs.sh
+++ scripts/issue-certs.sh
@@ -1,13 +1,13 @@
 # https://certbot-dns-cloudflare.readthedocs.io/en/stable/
 
 # sudo snap install --classic certbot
-# ln -s /snap/bin/certbot /usr/bin/certbot
+# sudo ln -s /snap/bin/certbot /usr/bin/certbot
 # sudo snap set certbot trust-plugin-with-root=ok
-# snap install --beta certbot-dns-cloudflare
+# sudo snap install --beta certbot-dns-cloudflare
 # sudo snap connect certbot:plugin certbot-dns-cloudflare
 
 sudo certbot certonly \
   --dns-cloudflare \
-  --dns-cloudflare-credentials ~/.secrets/cloudflare.ini \
+  --dns-cloudflare-credentials ~/.secrets/certbot/cloudflare.ini \
   --dns-cloudflare-propagation-seconds 60 \
   -d "*.ivan.build"
---
+++ scripts/renew-certificates.sh
@@ -0,0 +1,4 @@
+# This script is part of the following Cron job
+# 0 0 15 */3 * /root/renew-certificates.sh >/dev/null 2>&1
+
+certbot renew
